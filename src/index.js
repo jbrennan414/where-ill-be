b7b7255acbef89e65cc082f495693dc66264572f
John Brennan <jpb55116@gmail.com>
2020-02-17 19:03:54 -0700

    add google analtyics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactGA from 'react-ga';
+
+function initializeReactGA() {
+    ReactGA.initialize('UA-158701369-1');
+    ReactGA.pageview('/homepage');
+}
+
+initializeReactGA();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
